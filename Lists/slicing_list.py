717602d240901bf5bfd6fdb4a58fc1c3f9b4bfc6
PulkitGautam <pulkitgautam02@gmail.com>
2020-12-10 21:50:42 +0530

    tuples and other

--- Lists/slicing_list.py
+++ Lists/slicing_list.py
@@ -7,3 +7,10 @@ print(digit[1:3])
 #        list_name(start : end+1 : skip)
 #                                   omit start or end to slice list from beginning or end
 #                                   negative index can be used
+
+# Slicing can be used for copying List
+list2 = digit[:]                   # This method creates two separate List
+print(list2)                       # Changes don't reflect
+
+list3 = digit                      # This method associates list to other var
+print(list3)                       # Change in one reflects to another
---
+++ Lists/tuples.py
@@ -0,0 +1,5 @@
+# Tuples are List whose Values do not change throughout the Program(Immutable List)
+marks = (97, 95, 92, 98, 95)               # Round brackets
+
+# Rest Functions remains same
+# Changing Value gives error
