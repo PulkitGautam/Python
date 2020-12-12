c2d880600d7ed8636aaec799239ef759186c80bb
PulkitGautam <pulkitgautam02@gmail.com>
2020-12-12 10:47:16 +0530

    Changes

---
+++ 05 Dictionaries/loop_in_dictionary.py
@@ -0,0 +1,15 @@
+# Using for Loop through Dictionary
+Employee = {
+    'Name': "Ravi Kumar",
+    'Employee No': 25,
+    'Department': "Financial"
+}
+
+for key, value in Employee.items():
+    print(f"{key} of employee is {value} \n")
+
+# Syntax
+#                 for key_var, value_var in dict-name.items():
+#                    /...  Body .../
+         
+# If only one variable is given it is taken for key
