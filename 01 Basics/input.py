c7dd59b8a996807414f226bde185e3a5902e86e8
PulkitGautam <pulkitgautam02@gmail.com>
2020-12-13 12:47:10 +0530

    Input and While Loop

---
+++ 01 Basics/input.py
@@ -0,0 +1,10 @@
+# input() can be used to take input from user and store it in a variable
+Name = input("Enter Your Name:\t")
+print(Name)
+
+# Syntax
+#                    variable = input("message to be displayed")
+
+# Input taken is always string, use int() for converting numerical output
+Age = int(input("Enter Your Name:\t"))
+print(Age)
---
+++ 03 Loop/while_loop.py
@@ -0,0 +1,10 @@
+# While Loop
+x = 1
+while x <= 10:
+    print(x)
+    x += 1
+
+# Syntax 
+#              while /... Condition .../:
+#                /... Body of Loop .../
+#                /... Updation if any .../
